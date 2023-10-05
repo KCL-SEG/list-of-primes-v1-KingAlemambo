fcacb652492ef098eba5057b49db6e9a454c3658
alemambo <alexandros.mathios@outlook.com>
2023-10-05 13:43:38 +0100

    primes vol1 exercise

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def is_prime(num, prime_list):
+     for prime in prime_list:   
+        if num % prime == 0:
+            return False
+     return True
+     
+  
 def primes(number_of_primes):
-    list = []
-    return list
+   prime_list = []
+   num=2
+   while len(prime_list) < number_of_primes:
+        if len(prime_list)==0 or is_prime(num, prime_list):
+            prime_list.append(num)
+        num+=1
+   return prime_list
+
+
